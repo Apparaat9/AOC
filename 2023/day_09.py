ed7656d0e3260e67971af8fedda7a825eba04cc1
Apparaat9 <apparaat9@gmail.com>
2023-12-09 09:55:54 +0100

    Day 9 Final

--- 2023/day_09.py
+++ 2023/day_09.py
@@ -1,14 +1,11 @@
 
-data = list(map(lambda x:  list(map(int, x.split())), open('input/day9.txt').read().splitlines()))
+data = [[int(x) for x in l.split()] for l in open('input/day9.txt').read().splitlines()]
 
 def get_sequence(list):
-    global a
     if len(set(list)) == 1:
-        return list[0] if a else list[-1]
+        return list[-1]
     new_list = [list[i+1] - list[i] for i in range(len(list[:-1]))]
-    return list[0] - get_sequence(new_list) if a else list[-1] + get_sequence(new_list)
+    return list[-1] + get_sequence(new_list)
 
-a = 0
 print(sum(get_sequence(seq) for seq in data))
-a = 1
-print(sum(get_sequence(seq) for seq in data))
+print(sum(get_sequence(seq[::-1]) for seq in data))
