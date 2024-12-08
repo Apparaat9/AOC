9c28a9f69b5149411d87db0e9a189a7917ffec13
Apparaat Negen <apparaat9@gmail.com>
2024-12-08 19:04:21 +0100

    Day 08

--- 2024/day_08.py
+++ 2024/day_08.py
@@ -1,28 +1,20 @@
 from collections import defaultdict
 from itertools import permutations
 
+G = defaultdict(set)
 
-data = {complex(i, t):y for i, x in enumerate(open('input/day_08.txt').read().split()) for t, y in enumerate(x)}
+for i, x in enumerate(open('input/day_08.txt')):
+    for t, y in enumerate(x.strip()):
+        G[y].add(complex(i, t))
 
-G = defaultdict(list)
-all = set(data.keys())
-
-for coord, s in data.items():
-    G[s] += [coord]
-
-G.pop('.')
+all = G.pop('.') | {y for x in G.values() for y in x}
 
 def do_run(q):
     an = set()
     for coords in G.values():
         for a, b in permutations(coords, 2):
-            if q: an.add(a)
-            adiff = (a-b)
-            while(a in all):
-                a = a+adiff
-                an.add(a)
-                if not q: break
+            an |= set(a+n*(a-b) for n in range(*q))
     return len(all.intersection(an))
 
-print(do_run(0))
-print(do_run(1))
+print(do_run((1,2)))
+print(do_run((0 ,50)))
