0a97acd9bda04c501849e55288ce32d433f9454a
Apparaat Negen <apparaat9@gmail.com>
2024-12-11 21:09:25 +0100

    Day 11 cr

--- 2024/day_11.py
+++ 2024/day_11.py
@@ -9,20 +9,21 @@ def check_stone(stone, n):
     if stone == '0':
         return check_stone('1', n+1)
     elif not len(stone) % 2:
-        return check_stone(stone[:len(stone)//2], n+1) + check_stone(str(int(stone[len(stone)//2:])), n+1)
+        return check_stone(stone[:len(stone)//2], n+1) + \
+               check_stone(str(int(stone[len(stone)//2:])), n+1)
     else:
         return check_stone(str(int(stone)*2024), n+1)
 
 N = 25
-print(sum(x for x in [check_stone(s, 0) for s in stones]))
+print(sum([check_stone(s, 0) for s in stones]))
 check_stone.cache_clear()
 N = 75
-print(sum(x for x in [check_stone(s, 0) for s in stones]))
+print(sum([check_stone(s, 0) for s in stones]))
 
 ##### -- Momument of my own brilliant but unsuccessful, "smarter than cache" cache.
-    # from collections import defaultdict 
-    # from bisect import bisect
-    # cache_dict = defaultdict(dict)
+ #### from collections import defaultdict 
+  ### from bisect import bisect
+   ## cache_dict = defaultdict(dict)
     # if stone in cache_dict and bisect(list(cache_dict[stone]), N-n):
     #     target = bisect(list(cache_dict[stone]), N-n)
     #     if target:
