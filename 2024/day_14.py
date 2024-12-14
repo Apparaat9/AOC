f2d90c7a3df139d83be33dc73dffcf03d9fb958d
Apparaat Negen <apparaat9@gmail.com>
2024-12-14 19:29:53 +0100

    Day 14 mod

--- 2024/day_14.py
+++ 2024/day_14.py
@@ -3,16 +3,7 @@ from collections import Counter
 from functools import reduce
 
 def do_step(c, d):
-    nc = c + d
-    if nc.real >= H:
-        nc -= H
-    if nc.real < 0:
-        nc += H
-    if nc.imag >= W:
-        nc -= W * 1j
-    if nc.imag < 0:
-        nc += W * 1j
-    return nc
+    return (c.real + d.real) % H + (c.imag + d.imag) % W * 1j
 
 W, H = 101, 103
 
@@ -28,8 +19,3 @@ for i in range(10_000):
     if i == 99:
         qv = Counter([(x.real <  H//2, x.imag < W//2) for x in current if x.real != H//2 and x.imag != W//2])
         print(reduce(lambda x, y: x*y, qv.values()))
-
-
-
-
-
