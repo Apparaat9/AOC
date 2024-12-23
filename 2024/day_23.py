abbd8264dc72447cd8a90f97d4cd0478b73df56b
Apparaat Negen <apparaat9@gmail.com>
2024-12-23 18:15:19 +0100

    Day 24 sc

--- 2024/day_23.py
+++ 2024/day_23.py
@@ -7,13 +7,11 @@ for link in open('input/day_23.txt'):
     for x in conn:
         pcs[x] |= conn - {x}
 
-print(sum(all(a in pcs[b] for a, b in permutations(trio, 2))for trio in filter(lambda x : any(y.startswith('t') for y in x), combinations(pcs.keys(), 3))))
+print(sum(all(a in pcs[b] for a, b in permutations(trio, 2)) for trio in combinations(pcs.keys(), 3) if any(y.startswith('t') for y in trio)))
 
 clique = defaultdict(set)
 for k, conns in pcs.items():
-    connectivity = set()
     for x in conns:
-        connectivity |= conns & pcs[x]
-    clique[k] = connectivity | {k}
+        clique[k] |= conns & pcs[x] | {k}
 
 print(*sorted(max(clique.values(), key=lambda x : list(clique.values()).count(x))), sep=',')
