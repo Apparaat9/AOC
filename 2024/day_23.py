51a57ec18a40e9bbf39f74eb0b6ef446fef501cb
Apparaat Negen <apparaat9@gmail.com>
2024-12-23 18:28:55 +0100

    Day 24 gb

--- 2024/day_23.py
+++ 2024/day_23.py
@@ -7,7 +7,10 @@ for link in open('input/day_23.txt'):
     for x in conn:
         pcs[x] |= conn - {x}
 
-print(sum(all(a in pcs[b] for a, b in permutations(trio, 2)) for trio in combinations(pcs.keys(), 3) if any(y.startswith('t') for y in trio)))
+print(sum(all(a in pcs[b] 
+              for a, b in permutations(trio, 2)) 
+              for trio in combinations(pcs, 3) 
+              if any(y.startswith('t') for y in trio)))
 
 clique = defaultdict(set)
 for k, conns in pcs.items():
